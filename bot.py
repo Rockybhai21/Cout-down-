551b44a3cf3ebb099ef79afa144890397213eade
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-02-14 14:52:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # Bot Token from environment variable (Koyeb)
-TOKEN = os.getenv("7207793925:AAFME_OkdkEMMcFd9PI7cuoP_ahAG9OHg7U")
+TOKEN = os.getenv("")
 
 # Store user input time
 user_time = {}
