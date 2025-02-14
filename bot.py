c12946c1dea5a8d67f478dbfe58c7e74b57a6505
Rockybhai21 <rockybhai2109rs@gmail.com>
2025-02-14 14:58:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ def format_time(seconds):
 
 # Run bot
 def main():
-    app = Application.builder().token(TOKEN).build()
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, countdown_input))
     app.add_handler(CallbackQueryHandler(confirm_countdown, pattern=r"confirm_\d+"))
